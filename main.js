9ed99b972f6ad444e8d80b03cbed20d861aaf23f
shi0n0 <shiono.yui1080@gmail.com>
2023-04-28 23:55:50 +0900

    function addition

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getWeather() {
+function sendWeatherToSlack() {
     var api_key = "d87475d53bc05bed8e20e9e0c1d08206";
     var city = "Tokyo";
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key;
@@ -7,6 +7,23 @@ function getWeather() {
     var data = JSON.parse(json);
     var weather = data.weather[0].description;
     var temperature = Math.round(data.main.temp - 273.15);
-    var result = "今日の" + city + " の天気は" + weather + "です\n気温は" + temperature + "度になるでしょう。";
-    return result;
+    var message = "今日の" + city + " の天気は" + weather + "です\n気温は" + temperature + "度になるでしょう。";
+
+    var slackUrl = "slackUrl"
+    var payload = {
+        "text": message
+    };
+    var options = {
+        "method": "POST",
+        "payload": JSON.stringify(payload)
+    };
+
+    // 定期的に実行するためにトリガーを設定
+    ScriptApp.newTrigger("sendWeatherToSlack")
+    .timeBased()
+    .atHour(8)
+    .everyDays(1)
+    .create();
+
+UrlFetchApp.fetch(slackUrl, options);
 }
