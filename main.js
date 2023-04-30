aa76724308a8fc2fc1ca93171318eb90a962a029
shi0n0 <shiono.yui1080@gmail.com>
2023-04-30 23:58:27 +0900

    Trigger modification

--- main.js
+++ main.js
@@ -8,22 +8,47 @@ function sendWeatherToSlack() {
     var weather = data.weather[0].description;
     var temperature = Math.round(data.main.temp - 273.15);
     var message = "今日の" + city + " の天気は" + weather + "です\n気温は" + temperature + "度になるでしょう。";
-
-    var slackUrl = "slackUrl"
+  
+    var slackUrl = "https://hooks.slack.com/services/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"
     var payload = {
-        "text": message
+      "text": message
     };
     var options = {
-        "method": "POST",
-        "payload": JSON.stringify(payload)
+      "method": "POST",
+      "payload": JSON.stringify(payload)
     };
-
-    // 定期的に実行するためにトリガーを設定
+  
+    // トリガーを設定
+    var now = new Date();
+    var eightAM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8);
     ScriptApp.newTrigger("sendWeatherToSlack")
-    .timeBased()
-    .atHour(8)
-    .everyDays(1)
-    .create();
+      .timeBased()
+      .at(eightAM)
+      .create();
+  
+    UrlFetchApp.fetch(slackUrl, options);
+  }
+  
+  function addTime() {
+  const functionName = 'sendWeatherToSlack';
+  const date = new Date();
+  const time = '09:30';
+  date.setHours(...time.split(':'));
+  setTrigger(functionName, date);
+}
 
-UrlFetchApp.fetch(slackUrl, options);
+function deleteTrigger(functionName) {
+  const triggers = ScriptApp.getProjectTriggers();
+  triggers.forEach(trigger => {
+    if (trigger.getHandlerFunction() !== functionName) return;
+    ScriptApp.deleteTrigger(trigger);
+  })
+}
+
+//時間を指定してトリガーを設定
+function setTrigger(functionName, date) {
+  ScriptApp.newTrigger(functionName).
+    timeBased().
+    at(date).
+    create();
 }
